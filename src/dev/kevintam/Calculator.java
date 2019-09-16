e95bcd3b9cb91c25b28a89cdb05886b0360eca5e
KevinTamCS <kevin@kevintam.dev>
2019-09-16 00:30:12 -0400

    Implement calculator UI.

---
+++ src/dev/kevintam/Calculator.java
@@ -0,0 +1,11 @@
+package dev.kevintam;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        System.out.println("Loading Java Calculator...");
+
+        // Create the calculator UI
+        CalculatorUI calculator = new CalculatorUI();
+    }
+}
